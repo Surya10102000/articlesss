6f565c31a28f1a5309a38db24f78c57e2cf53b38
Surya Prakash Mallah <sfrost488@gmail.com>
2024-09-18 03:41:23 +0530

    created Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import Options from "./Navbar/Options";
+import { useAuthCheck } from "@/hooks/useAuthCheck";
+import { useRecoilValue } from "recoil";
+import { userState } from "@/recoil/atom";
 
 const Navbar = () => {
-  let token = localStorage.getItem('token');
+  const loading = useAuthCheck();
+  const user = useRecoilValue(userState);
+
   return (
     <header className="flex justify-between p-4">
-        <Link className="font-extrabold text-2xl" to="/"> logo </Link>
-        <nav className="flex gap-2">
-            <Link to="/"><Button variant='link'>Home</Button></Link>
-            {token ? <Options/> : <Link to="/login"><Button >Join</Button></Link> }
-        </nav>
+      <Link className="font-extrabold text-3xl" to="/">
+        logos
+      </Link>
+      <nav className="flex gap-2">
+        <Link to="/">
+          <Button variant="link">Home</Button>
+        </Link>
+        {!loading &&
+          (user ? (
+            <Options />
+          ) : (
+            <Link to="/login">
+              <Button>Login</Button>
+            </Link>
+          ))}
+      </nav>
     </header>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
--- client/src/components/Navbar/Options.tsx
+++ client/src/components/Navbar/Options.tsx
@@ -2,25 +2,51 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
-  DropdownMenuItem
+  DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "../ui/button";
-// import { useRecoilValue } from "recoil";
-// import { userAuthState } from "@/recoil/atom";
+import { useRecoilState } from "recoil";
+import { userState } from "@/recoil/atom";
+import { useState } from "react";
+import axios from "axios";
 
 const Options = () => {
+  const [user, setUser] = useRecoilState(userState);
+  const [loading, setLoading] = useState(false);
 
-    // const userAuth = useRecoilValue(userAuthState)
+  async function logout() {
+    try {
+      if (loading) {
+        return;
+      }
+      setLoading(true);
+      await axios.delete(
+        "http://localhost:3000/api/auth/logout",
+        { withCredentials: true }
+      );
+      console.log("user session deleted");
+
+      setUser(null);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false)
+      console.log(error);
+    }
+  }
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild><Button variant='outline'>menu</Button></DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">menu</Button>
+      </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>Profile</DropdownMenuItem>
         <DropdownMenuItem>Manage Blogs</DropdownMenuItem>
-        {/* {userAuth?.role === "admin" && (
-          <DropdownMenuItem>Admin</DropdownMenuItem>
-        )} */}
+        {user && (
+          <DropdownMenuItem onClick={() => logout()} className="text-red-400">
+            Logout
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
