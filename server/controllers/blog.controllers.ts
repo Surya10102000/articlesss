2085bf572de404bb6311fa38ee01c26df21ddc60
Surya Prakash Mallah <sfrost488@gmail.com>
2024-09-27 03:17:03 +0530

    added getBlog route(with pagination) & create blog route

--- server/controllers/blog.controllers.ts
+++ server/controllers/blog.controllers.ts
@@ -44,3 +44,57 @@ export const publishPost = ( req : Request , res : Response) =>{
     });
   }
 }
+
+
+export const getBlog = async (req : Request, res: Response)=>{
+  try {
+    const page = parseInt(req.query.page as string) || 1; 
+    const pageSize = 5; 
+
+    const totalBlogs = await BlogPost.countDocuments();
+
+    // {published : true}
+    const blogs = await BlogPost.find()
+    .sort({ createdAt: -1 }) 
+    .skip((page - 1) * pageSize)
+    .limit(pageSize)
+    .select("-content");
+    console.log(blogs)
+    const totalPages = Math.ceil(totalBlogs / pageSize);
+
+    res.json({ blogs, totalPages, currentPage: page });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch blogs' });
+  }
+}
+
+export const createDraft = async (req : Request , res : Response)=>{
+  try {
+    const validatedData = saveBlogPostSchema.parse(req.body)
+
+    const userId = (req.user as any)?._id?.toString();
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    const newBlog = new BlogPost({
+      ...validatedData,
+      author: userId,
+    });
+
+    const savedBlog = await newBlog.save()
+
+    return res.status(201).json({
+      message: "Blog created successfully",
+      blog: savedBlog,
+    });
+  } catch (error : any) {
+    if (error instanceof ZodError) {
+      return res.status(400).json(handleZodError(error));
+    }
+
+    return res.status(500).json({
+      error: error.message || "Internal server error",
+    });
+  }
+}
--- server/routes/blog.routes.ts
+++ server/routes/blog.routes.ts
@@ -1,33 +1,40 @@
-import express from "express"
-import { ensureAuthenticated } from "../middleware/ensureAuthenticated.middleware"
-import { createBlog, publishPost } from "../controllers/blog.controllers"
+import express, { Request, Response } from "express";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated.middleware";
+import {
+  createBlog,
+  createDraft,
+  getBlog,
+  publishPost,
+} from "../controllers/blog.controllers";
+import { BlogPost } from "../models/blogPost.model";
 
-const router = express()
+const router = express();
 
-router.post('/demo', ensureAuthenticated, (req, res)=>{
-    const result = "isauthenticated"
-    const { title , summary , content} = req.body
-    res.json({ message : result,
-        request : req.user,
-       title : title.length,
-       summary : summary.length,
-       content : content.length
-    })
-})
+router.post("/demo", ensureAuthenticated, (req, res) => {
+  const result = "isauthenticated";
+  const { title, summary, content } = req.body;
+  res.json({
+    message: result,
+    request: req.user,
+    title: title.length,
+    summary: summary.length,
+    content: content.length,
+  });
+});
 
-router.post("/create",ensureAuthenticated, createBlog); //create a new blog
-router.post("/publish/:id",ensureAuthenticated, publishPost) // publish blog post
+router.post("/create", ensureAuthenticated, createBlog); //create a new blog
+router.post("/publish/:id", ensureAuthenticated, publishPost); // publish blog post
 // router.post("/save/:id", saveBlog) //Save blog post as draft
 // router.patch("/update/:id", updateBlog) // Update a specific fields of a blog post
-// router.delete("/delete/:id", deleteBlog) 
+// router.delete("/delete/:id", deleteBlog)
 // router.get("/:id", getPost)
 // router.get("blogs", getPosts)
 
+router.get("", getBlog);
+router.post("/draft", ensureAuthenticated, createDraft);
 
 // TODO :
 // 1. article can be saved to the db - on controller for that
 // 2. article publish controller
 
-
-
-export default router
+export default router;
